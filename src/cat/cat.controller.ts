f98f03b92687211e20ba0dda1da267f610b140a8
yuki <yuki.onishi1129@gmail.com>
2021-09-19 20:21:39 +0900

    add: router controller

--- src/cat/cat.controller.ts
+++ src/cat/cat.controller.ts
@@ -2,8 +2,22 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('cat')
 export class CatController {
+  // @Get(): getパラメータのメソッドに指定
+  // Getのステータスコードは200
+  // POSTは201
+  // @HttpCode(...)でステータスコードを変更できる
   @Get()
+  // メソッド名は任意
   findAll(): string {
+    // オブジェクトを返す場合、自動的にjsonにシリアライズされる
+    // プリミティブ型(string, number, boolean)を返す場合は値だけを返す
     return 'This action returns all cats';
+    // return [1, 2, 3];
+  }
+
+  // /cat/profileのルーティング
+  @Get('profile')
+  findProfile(): string {
+    return 'This  action returns cats profile';
   }
 }
