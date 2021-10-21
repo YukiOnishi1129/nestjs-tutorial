195801a2558c7a3ac24756c4efc4b43a1392d426
yuki <yuki.onishi1129@gmail.com>
2021-10-22 01:29:25 +0900

    add: global

--- src/cat/cat.module.ts
+++ src/cat/cat.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 /* controller */
 import { CatController } from './cat.controller';
 /* service */
 import { CatService } from './cat.service';
 
+// @Global()
+// Globalを定義すると、どのモジュールからも利用できるようになる (モジュールをグローバル化する)
 @Module({
   controllers: [CatController],
   providers: [CatService],
+  exports: [CatService], // CatServiceのインスタンスを複数のモジュールで共有する際の設定
+  // これでCatModuleをimportしたモジュールはCatServiceにアクセスできるようになる
 })
 export class CatModule {}
