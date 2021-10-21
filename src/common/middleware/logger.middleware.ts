02fa80a6f98c6c486d8d0860351ca24db4093f9c
yuki <yuki.onishi1129@gmail.com>
2021-10-22 01:37:41 +0900

    add: logger middleware

---
+++ src/common/middleware/logger.middleware.ts
@@ -0,0 +1,10 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+// Middlewareを使用するには、関数かInjectable　デコレーターを持つクラスのいずれかを使用する
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    next();
+  }
+}
