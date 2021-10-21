62e2794dd4189a4b33482fd728f36141a1e3df45
yuki <yuki.onishi1129@gmail.com>
2021-10-22 01:43:35 +0900

    add: middleware at app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 /* modules */
 import { CatModule } from './cat/cat.module';
+/* middleware */
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+/* controllers */
+import { AppController } from './app.controller';
+/* services */
+import { AppService } from './app.service';
 
 @Module({
   imports: [CatModule], // 他ドメインのモジュールを定義
   controllers: [AppController],
   providers: [AppService], // providerを定義
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // catルートハンドラにLoggerMiddlewareを定義
+    // forRoutesにルートパスやリクエストメソッドを含むオブジェクトを渡すことで、
+    // ミドルウェアを特定のリクエストメソッドに制限できる
+    // urlが/cat/~のルートのみミドルウェアを適用させる
+    consumer.apply(LoggerMiddleware).forRoutes('cat');
+  }
+}
